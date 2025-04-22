1a64c159fba45803b5869e64333330b21cd92274
Zeph26 <159867529+Zeph26@users.noreply.github.com>
2025-04-22 15:05:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,207 @@
+
+// // var alphabet_image = ""
+
+// // function new_alphabets(get_alphabet){
+// //     fabric.Image.fromURL(get_alphabet, function (Img){
+// //         console.log("Image loaded:", get_alphabet);
+// //         alphabet_image = Img;
+// //         // alphabet_image.scaleToWidth(400);
+// //         // alphabet_image.scaleToHeight(400);
+// //         alphabet_image.scale(2)
+// //         alphabet_image.set({ top: 50, left: 50 });
+// //         canvas.add(alphabet_image);
+
+// //     });
+// // }
+
+// var canvas = new fabric.Canvas('c');
+
+// fabric.Image.fromURL("c.png", function(Img){
+//     console.log("its loaded")
+//     Img.scaleToWidth(150);
+//     Img.scaleToHeight(140);
+//     Img.set({
+//         top:100,
+//         left:100
+//     })
+//     canvas.add(Img)
+// }) 
+
+// // fabric.Image.fromURL('abc.png', function(img) {
+  
+// //   img.set({
+// //     left: 200,
+// //     top: 200,
+// //     scaleX: 2,   
+// //     scaleY: 2 
+// //   });
+ 
+// //   canvas.add(img);
+  
+// // });
+// // window.addEventListener("keydown", my_keydown);
+
+// // function my_keydown(e){
+// //     console.log("in func")
+// //     keyPressed = e.key.toLowerCase();
+
+// //     if(keyPressed == "a"){
+// //         new_alphabets("a.png");
+// //         console.log("a pressed");
+        
+// //     }
+    
+// //     // for b
+// //     if(keyPressed == "b"){
+// //         new_alphabets("b.png");
+// //     }
+    
+    
+// //     //for c 
+// //     if(keyPressed == "c"){
+// //         new_alphabets("c.png");
+// //     }
+    
+    
+// //     //for d
+// //     if(keyPressed == "d"){
+// //         new_alphabets("d.png");
+   
+// //     }
+
+
+    
+// //     if(keyPressed == "e"){
+// //         new_alphabets("e.png");
+
+
+// //     }
+    
+    
+    
+// //     if(keyPressed == "f"){
+// //         new_alphabets("f.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "g"){
+// //         new_alphabets("g.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "h"){
+// //          new_alphabets("h.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "i"){
+// //         new_alphabets("i.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "j"){
+// //          new_alphabets("j.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "k"){
+// //          new_alphabets("k.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "l"){
+// //         new_alphabets("l.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "m"){
+// //         new_alphabets("m.png")
+// //     }
+    
+    
+    
+// //     if(keyPressed == "n"){
+// //         new_alphabets("n.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "o"){
+// //         new_alphabets("o.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "p"){
+// //         new_alphabets("p.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "q"){
+// //         new_alphabets("q.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "r"){
+// //         new_alphabets("r.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "s"){
+// //        new_alphabets("s.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "t"){
+// //         new_alphabets("t.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "u"){
+// //         new_alphabets("u.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "v"){
+// //         new_alphabets("v.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "w"){
+// //         new_alphabets("w.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "x"){
+// //         new_alphabets("x.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "y"){
+// //         new_alphabets("y.png");
+// //     }
+    
+    
+    
+// //     if(keyPressed == "z"){
+// //         new_alphabets("z.png");
+// //     }
+// // }
+
+
